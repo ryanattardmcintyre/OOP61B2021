c7e86bdffeef840c6436d18f27936b905cbb5109
Ryan Attard <ryanattard@gmail.com>
2021-11-04 12:53:38 +0100

    Class Relationshops: Association

---
+++ Week5_ClassRelationships/House.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week5_ClassRelationships
+{
+    class House
+    {
+        public string Address { get; set; }
+        public double SqM { get; set; }
+
+        public List<Room> Rooms { get; set; } // this is association 
+
+    }
+
+    class Room
+    {
+        public string Type { get; set; }
+        public double SqM { get; set; }
+
+        public bool Ensuite { get; set; }
+    }
+}
---
+++ Week5_ClassRelationships/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week5_ClassRelationships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
