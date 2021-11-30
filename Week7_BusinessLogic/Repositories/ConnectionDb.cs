aaf79f5ff8cbce9dc89eabcc2a6930cede1832b3
Ryan Attard <ryanattard@gmail.com>
2021-11-30 10:33:29 +0100

    Created Connection class

---
+++ Week7_BusinessLogic/Repositories/ConnectionDb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week7_BusinessLogic.Context;
+
+namespace Week7_BusinessLogic.Repositories
+{
+    public class ConnectionDb
+    {
+        public ShoppingCartEntities Entity { get; set; }
+
+        public ConnectionDb()
+        {
+            Entity = new ShoppingCartEntities();
+        }
+
+    }
+}
